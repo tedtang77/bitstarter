ebda6a1c1cef76b83423d13f3f8ecfc33df37c5d
Ubuntu <ubuntu@ip-172-31-6-118.ap-southeast-1.compute.internal>
2015-12-21 13:56:33 +0000

    Finish HW3 Part 1

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+/*
 var fs = require( 'fs' );
 var input = "index.html";
 var buf = new Buffer( fs.readFileSync( input ) ); 
-
+*/
 
 app.get('/', function(request, response) {
-  /* response.send('Hello World 2!') */
-  response.send( buf.toString( 'utf8' ) )
+  response.send('Hello World 2!')   
+  /*response.send( buf.toString( 'utf8' ) )*/
 })
 
 app.listen(app.get('port'), function() {
