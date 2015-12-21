118f36ab793afaacef937b819c5260bf8898fd35
Ubuntu <ubuntu@ip-172-31-6-118.ap-southeast-1.compute.internal>
2015-12-21 14:16:22 +0000

    Finish HW3 part 1

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 var express = require('express')
 var app = express()
 
@@ -10,13 +8,14 @@ app.use(express.static(__dirname + '/public'))
 var fs = require( 'fs' );
 var input = "index.html";
 var buf = new Buffer( fs.readFileSync( input ) ); 
-
+var output = buf.toString( 'utf8' );
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')   
-  response.send( buf.toString( 'utf8' ) )
+  /*response.send('Hello World 2!')*/   
+  response.send( output );
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+  console.log(output); 
 })
